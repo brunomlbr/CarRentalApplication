af05c3b005076c4b05343efb8d70e193f5de3418
Bruno Leite <brunomlbr@gmail.com>
2021-06-15 18:55:41 -0300

    chapter 5 start

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -10,27 +10,27 @@ let connection: Connection;
 
 describe("List Categories", () => {
     beforeAll(async () => {
-        connection = await createConnection();
-        await connection.runMigrations();
-
+        // connection = await createConnection();
+        // await connection.runMigrations();
         // const id = uuidV4();
         // const password = await hash("admin", 8);
-
         // await connection.query(`INSERT INTO USERS ( id, name, email, password, "isAdmin", created_at, driver_license)
         // values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXX' ) `);
     });
 
     afterAll(async () => {
-        await connection.dropDatabase();
-        await connection.close();
+        // await connection.dropDatabase();
+        // await connection.close();
         // await new Promise((resolve) => setTimeout(() => resolve(), 500)); // avoid jest open handle error
     });
 
     it("Should be able to list categories", async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
         const id = uuidV4();
         const password = await hash("admin", 8);
 
-        await connection.query(`INSERT INTO USERS ( id, name, email, password, "isAdmin", created_at, driver_license) 
+        await connection.query(`INSERT INTO USERS ( id, name, email, password, "isAdmin", created_at, driver_license)
         values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXX' ) `);
         // await request(app).get("/cars/available").expect(200);
         const responseToken = await request(app).post("/sessions").send({
@@ -72,9 +72,9 @@ describe("List Categories", () => {
             });
 
         const response = await request(app).get("/categories");
-
-        console.log(response.body);
-
+        // console.log(response.body);
+        await connection.dropDatabase();
+        await connection.close();
         expect(response.status).toBe(200);
         expect(response.body.length).toBe(3);
         expect(response.body[0]).toHaveProperty("id");
